a8522ca2c7fdf97c5ba84af7006e386a7c2ade33
Sweta  Bhandari <bhandarisweta1@gmail.com>
2023-07-31 16:37:16 +0545

    updated

---
+++ app/Http/Controllers/PostCrudController.php
@@ -0,0 +1,25 @@
+
+
+<!-- // namespace App\Http\Controllers\Admin;
+
+// use Backpack\CRUD\app\Http\Controllers\CrudController;
+
+// class PostCrudController extends CrudController
+// {
+//     protected $model = 'App\Models\Post';
+
+//     protected $crudName = 'Posts';
+
+//     protected $fields = [
+//         'title',
+//         'content',
+//         'published_at',
+//     ];
+
+//     protected $validationRules = [
+//         'title' => 'required|string|max:255',
+//         'content' => 'required|string',
+//         'published_at' => 'nullable|date',
+//         'author_id' =>   'required'
+//     ];
+}  -->
